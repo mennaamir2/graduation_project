0c20f17ed4309dc34072be6651598a64ae71ca7a
Menna Amir <mennaamir72@gmail.com>
2024-12-14 08:23:58 +0200

    edit doctor_offers file

--- lib/features/home/presentation/views/widgets/doctors_offers.dart
+++ lib/features/home/presentation/views/widgets/doctors_offers.dart
@@ -9,9 +9,9 @@ class DoctorsOffers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 8.h),
+      padding: EdgeInsets.only(top: 7.h,bottom: 7.h),
       width: 342.w,
-      height: 81.h,
+      // height: 81.h,
       decoration: BoxDecoration(
         boxShadow: [
           BoxShadow(
@@ -27,45 +27,42 @@ class DoctorsOffers extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.only(left: 16.w),
+            padding: EdgeInsets.only(left: 16.w,right: 25.w),
             child: Image.asset(
               'assets/images/doctor_image.png',
               height: 67.h,
               width: 54.w,
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: 25.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Dr Ahmed Yasser',
-                  style: roboto14(),
-                ),
-                Text(
-                  'Dentist',
-                  style: textStyle12(
-                      color: const Color(0xff979797), letterSpacing: .07),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '300 EGP',
-                      style: textStyle10(letterSpacing: .03),
-                    ),
-                    SizedBox(
-                      width: 5.w,
-                    ),
-                    Text(
-                      '200 EGP',
-                      style: nunitoSans14(letterSpacing: .1),
-                    )
-                  ],
-                )
-              ],
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Dr Ahmed Yasser',
+                style: roboto14(),
+              ),
+              Text(
+                'Dentist',
+                style: textStyle12(
+                    color: const Color(0xff979797), letterSpacing: .07),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '300 EGP',
+                    style: textStyle10(letterSpacing: .03),
+                  ),
+                  SizedBox(
+                    width: 5.w,
+                  ),
+                  Text(
+                    '200 EGP',
+                    style: nunitoSans14(letterSpacing: .1),
+                  )
+                ],
+              )
+            ],
           ),
           Padding(
             padding: EdgeInsets.only(left: 73.w),
